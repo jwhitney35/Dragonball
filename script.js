eae3cb3af8e40ef442f085048f1bcc0047eec57f
Jonathon Whitney <JWhitney@iterna.local>
2019-05-08 08:37:20 -0500

    first changes

---
+++ script.js
@@ -0,0 +1,6 @@
+// this is the script jQuery file for the Ada's Goku webpage
+
+// make sure to run and jQuery code after the html is loaded
+//can also be written as $(function(){});
+$(function(){});
+
