6f9ab5a6218d994b76c96a90137af5e8161d73ac
Jonathon Whitney <JWhitney@iterna.local>
2019-05-10 09:13:53 -0500

    added songs and goku transformation

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 
 // make sure to run and jQuery code after the html is loaded
 //can also be written as $(function(){});
-$(function(){});
+$(function() {});
 
+$(".gokuBase").click(function() {
+    $(".gokuBase").fadeOut(500);
+    $(".gokuSS1").fadeIn(1000);
+});
+
+$(".gokuSS1").click(function() {
+    $(".gokuSS1").fadeOut("fast");
+    $(".gokuBase").fadeIn("slow");
+});
