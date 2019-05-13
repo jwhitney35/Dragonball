2f758ab8224b0805b9c29b63d886836ed71d6a0f
Jonathon Whitney <JWhitney@iterna.local>
2019-05-13 16:41:25 -0500

    added new features and sections

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ $("#BASE").click(function() {
     $(".gokuSS3").fadeOut("fast");
     $(".gokuGOD").fadeOut("fast");
     $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuBase").fadeIn("slow");
     $(".gokuBase").css("display", "block");
 
@@ -32,6 +33,7 @@ $("#SS1").click(function() {
     $(".gokuSS3").fadeOut("fast");
     $(".gokuGOD").fadeOut("fast");
     $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuSS1").fadeIn("slow");
     $(".gokuSS1").css("display", "block");
 
@@ -43,6 +45,7 @@ $("#SS2").click(function() {
     $(".gokuSS3").fadeOut("fast");
     $(".gokuGOD").fadeOut("fast");
     $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuSS2").fadeIn("slow");
     $(".gokuSS2").css("display", "block");
 
@@ -54,6 +57,7 @@ $("#SS3").click(function() {
     $(".gokuSS2").fadeOut("fast");
     $(".gokuGOD").fadeOut("fast");
     $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuSS3").fadeIn("slow");
     $(".gokuSS3").css("display", "block");
 
@@ -65,6 +69,7 @@ $("#GOD").click(function() {
     $(".gokuSS2").fadeOut("fast");
     $(".gokuSS3").fadeOut("fast");
     $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuGOD").fadeIn("slow");
     $(".gokuGOD").css("display", "block");
 
@@ -76,7 +81,20 @@ $("#BLUE").click(function() {
     $(".gokuSS2").fadeOut("fast");
     $(".gokuSS3").fadeOut("fast");
     $(".gokuGOD").fadeOut("fast");
+    $(".gokuSS4").fadeOut("fast");
     $(".gokuBLUE").fadeIn("slow");
     $(".gokuBLUE").css("display", "block");
+
+});
+
+$("#SS4").click(function() {
+    $(".gokuBase").fadeOut("fast");
+    $(".gokuSS1").fadeOut("fast");
+    $(".gokuSS2").fadeOut("fast");
+    $(".gokuSS3").fadeOut("fast");
+    $(".gokuGOD").fadeOut("fast");
+    $(".gokuBLUE").fadeOut("fast");
+    $(".gokuSS4").fadeIn("slow");
+    $(".gokuSS4").css("display", "block");
 
 });
